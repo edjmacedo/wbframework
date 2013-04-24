47fef697e19f30479bf9fb57384fb84a6f30d33f
Edvan Macedo <edvan.macedo.jr@gmail.com>
2013-04-24 19:02:00 -0400

    Added views layer Signed-off-by: Edvan Macedo <edvan.macedo.jr@gmail.com>

---
+++ views/home.js
@@ -0,0 +1,15 @@
+var wb = window.wb || {};
+
+wb.home = {
+	init: function(){
+		wb.home.render();
+		wb.home.bind();
+	},
+	bind: function(){
+	},
+	render: function(data){
+	},
+	destroy: function(){
+	}	
+};
+window.wb = wb;
