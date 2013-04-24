d204bfc70ea29958e3e4fbf539e07cb0ee85e825
Edvan Macedo <edvan.macedo.jr@gmail.com>
2013-04-24 19:02:51 -0400

    Added file for internationalization Signed-off-by: Edvan Macedo <edvan.macedo.jr@gmail.com>

---
+++ i18n.js
@@ -0,0 +1,25 @@
+var i18n = window.i18n || {};
+
+i18n.ptbr = {
+	'TEXT_HOME' : 'Principal',	
+	'TEXT_PROFILE' : 'Perfil',
+	'TEXT_FRIENDS' : 'Amigos',
+	'TEXT_EVENTS' : 'Eventos',	
+	'TEXT_ADD' : 'Adicionar Amigo',
+	'TEXT_OTHER_PLACES' : 'Outros Lugares',
+	'TEXT_SHARE' : 'Recomendar'
+};
+
+i18n.en = {
+	'TEXT_HOME' : 'Home',	
+	'TEXT_PROFILE' : 'Profile',	
+	'TEXT_FRIENDS' : 'Friends',
+	'TEXT_EVENTS' : 'Events',
+	'TEXT_ADD' : 'Add Friend',
+	'TEXT_OTHER_PLACES' : 'Other Places',
+	'TEXT_SHARE' : 'Recommends'	
+};
+
+i18n.enus = i18n.en;
+
+window.i18n = i18n;
